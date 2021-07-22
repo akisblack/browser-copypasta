45627310a564ae503a6172e90c9ecda8b64083d6
akisblack <41871089+akisblack@users.noreply.github.com>
2021-07-22 22:29:18 +0300

    ms edge best browser

--- src/App.svelte
+++ src/App.svelte
@@ -1,130 +1,53 @@
-<script>
-	window.addEventListener('input', function (e) {
-		document.getElementsByName("insert").forEach(function(value) {
-			value.innerHTML = document.getElementById("userInput").value;
-		})
-	}, false);
-</script>
-
 <main>
-	<header>
+    <header>
 		<h1 class="title">Browser Copypasta</h1>
 			<ul>
 				<div class="dropdown">
 					<button class="dropbtn">Switch Browser</button>
 					<div class="dropdown-content">
-						<a href="/">Link 1</a>
-						<a href="/">Link 2</a>
-						<a href="/">Link 3</a>
+						<a href="#/">Vivaldi</a>
+						<a href="/#/brave">Brave</a>
 					</div>
 				  </div> 
-				<button>Copy Text</button>
+                <button onclick="copyText()">Copy Text (broken)</button>
 			</ul>
 	</header>
 
-	<div id="input">
-		<input type="text" id="userInput" value="VIVALDI BROWSER">
-	</div>
+<!-- Show the router -->
+<Router {routes} />
 
-	<div id="text">
-		<p>For me, it's the <b name="insert">VIVALDI BROWSER</b>. The best internet browser. I even download the <b name="insert">VIVALDI BROWSER</b> on my phone, tablet, laptop, and desktop because the <b name="insert">VIVALDI BROWSER</b> is so user friendly and helps fill my workflow with awesome tab features (Tab Stacks & Tab Tilling)</p><br>
-
-		<p>One time I asked for 3 tab stacks, WITH the tab tilling (multiple sites open at once on a screen). I said, "Wow, three for free!" and the nice friendly <b name="insert">VIVALDI BROWSER</b> dev laughed and said, "I'm going to call you 3-for-free!". But the best part is with tab tilling, it's not even free for me!</p><br>
-
-		<p>Now the <b name="insert">VIVALDI BROWSER</b> greets me with "hey it's 3-for-free!" and ALWAYS gets me a tab stack, with a web panel on the side. It's such a fun and cool atmosphere with my <b name="insert">VIVALDI BROWSER</b>, I use it at least 7 times a week for my daily browsing needs which include work, shitposting, school, and stalking my ex on facebook (with her instagram tiled to the side!).</p><br>
-
-		<p>What a great <b name="insert">VIVALDI BROWSER</b></p>
-	</div>
-
+    <footer>
+        <div class="links">
+                <a href="https://github.com/cantinadevs/browsercopypasta"><i class="mdi mdi-github"></i></a>
+                <a href="https://cantinadevs.github.io"><i class="mdi mdi-web"></i></a>
+                <hr>
+                <p><i class="mdi mdi-copyright"></i> This site is maintained by The Cantina Devs and is licensed under the MIT License</p>
+        </div>
+    </footer>
 </main>
 
+<script>
+    // Import the router component
+    // Normally, this would be: `import Router from 'svelte-spa-router'`
+    import Router from '../Router.svelte'
 
-<style>
-/* Navbar*/
-.title {
-	color: aquamarine;
-}
+    // Import the list of routes
+    import routes from './routes'
+    
+    window.addEventListener('input', function (e) {
+    document.getElementsByName("insert").forEach(function(value) {
+        value.innerHTML = document.getElementById("userInput").value;
+    })
+    }, false);
 
-button {
-	margin-top: 5px;
-	background-color: aquamarine;
-	border-radius: 5px;
-	border: none;
-	height: 25px;
-	color: black;
-	padding-left: 20px;
-	padding-right: 20px;
-}
-
-@media screen and (max-width: 406px) {
-  button {
-    padding-bottom: 40px;
-	padding-top: 7px;
-  }
-}
-
-header {
-	margin: 0 auto;
-	max-width: 70%;
-	display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-header > ul {
-	float: right;
-}
-
- /* Dropdown*/
- .dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: black;
-  min-width: 25px;
-  z-index: 1;
-}
-.dropdown-content a {
-  color: #fff;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
-.dropdown-content a:hover {
-	background-color: #333333;
-}
-
-.dropdown:hover .dropdown-content {
-	display: block;
-}
-
-/* Content*/
-#input, #text {
-	margin: 0 auto;
-	max-width: 70%;
-}
-
-#text {
-	line-height: 25px;
-	text-align: justify;
-	color: #fff
-}
-
-#input {
-    padding-top: 70px;
-}
-
-input {
-    width: 100%;
-    background-color: #272727;
-    color: #fff;
-    border-radius: 15px;
-    border: 2px solid aquamarine;
-}
-
-</style>
+    function copyText() {
+        var content = document.getElementById("text").innerText; //get text from copypasta
+        try { // try this
+            navigator.clipboard.writeText(content); //copy the copypasta
+        }
+        catch { // something went wrong
+            console.log("We couldn't copy the copypasta. User-Agent:" + navigator.userAgent);
+            alert("Could not copy, please tell the creator of this site, include this: \n" + navigator.userAgent);
+        }
+    }
+</script>
---
+++ src/routes/Brave.svelte
@@ -0,0 +1,13 @@
+<div id="input">
+    <input type="text" id="userInput" value="BRAVE BROWSER®">
+</div>
+
+<div id="text">
+    <p>For me, it's the <b name="insert">BRAVE BROWSER®</b>. The best internet browser. I even download the <b name="insert">BRAVE BROWSER®</b> on my phone, tablet, laptop, and desktop because the <b name="insert">BRAVE BROWSER®</b> DEVELOPMENT TEAM is so friendly and more than willing to oblige.</p><br>
+
+    <p>One time I asked for 3 ads to be blocked and they blocked three. I said, "Wow, three for free!" and the nice friendly <b name="insert">BRAVE BROWSER®</b> worker laughed and said, "I'm going to call you 3-for-free!".</p><br>
+
+    <p>Now the <b name="insert">BRAVE BROWSER®</b> greets me with "hey it's 3-for-free!" and ALWAYS blocks every ad, cookie, and tracker. It's such a fun and cool atmosphere with my <b name="insert">BRAVE BROWSER®</b>, I use it at least 7 times a week for my daily browsing needs which include work, pornography, school, and stalking my whore ex wife on facebook.</p><br>
+
+    <p>I even use my <b name="insert">BRAVE BROWSER®</b> to browse Read It®, it's amazing! What a great <b name="insert">BRAVE BROWSER®</b>.</p>
+</div>
