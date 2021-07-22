b650e1b822518462704a25d44b45eee01892ac4f
Oxygemo <alej0hio2007@gmail.com>
2021-07-22 22:41:47 +0200

    Fix copy and add a fancy snackbar
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,37 @@
+<script>
+    // Import the router component
+    // Normally, this would be: `import Router from 'svelte-spa-router'`
+    import Router from '../Router.svelte'
+
+    // Import the list of routes
+    import routes from './routes'
+
+    window.addEventListener('input', function (e) {
+    document.getElementsByName("insert").forEach(function(value) {
+        value.innerHTML = document.getElementById("userInput").value;
+    })
+    }, false);
+
+    export function copyText() {
+        var content = document.getElementById("text").innerText; //get text from copypasta
+        try { // try this
+            navigator.clipboard.writeText(content); //copy the copypasta
+            // Get the snackbar DIV
+            var x = document.getElementById("snackbar");
+
+            // Add the "show" class to DIV
+            x.className = "show";
+
+            // After 3 seconds, remove the show class from DIV
+            setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+        }
+        catch { // something went wrong
+            console.log("We couldn't copy the copypasta. User-Agent:" + navigator.userAgent);
+            alert("Could not copy, please tell the creator of this site, include this: \n" + navigator.userAgent);
+        }
+    }
+</script>
+
 <main>
     <header>
 		<h1 class="title">Browser Copypasta</h1>
@@ -9,8 +43,11 @@
 						<a href="/#/brave">Brave</a>
 					</div>
 				  </div> 
-                <button onclick="copyText()">Copy Text (broken)</button>
+                <button on:click={copyText}>Copy Text (broken)</button>
 			</ul>
+            <div id="snackbar">
+                <p id="snackbarText">✔️Copied to clipboard.</p>
+            </div>
 	</header>
 
 <!-- Show the router -->
@@ -25,29 +62,3 @@
         </div>
     </footer>
 </main>
-
-<script>
-    // Import the router component
-    // Normally, this would be: `import Router from 'svelte-spa-router'`
-    import Router from '../Router.svelte'
-
-    // Import the list of routes
-    import routes from './routes'
-    
-    window.addEventListener('input', function (e) {
-    document.getElementsByName("insert").forEach(function(value) {
-        value.innerHTML = document.getElementById("userInput").value;
-    })
-    }, false);
-
-    function copyText() {
-        var content = document.getElementById("text").innerText; //get text from copypasta
-        try { // try this
-            navigator.clipboard.writeText(content); //copy the copypasta
-        }
-        catch { // something went wrong
-            console.log("We couldn't copy the copypasta. User-Agent:" + navigator.userAgent);
-            alert("Could not copy, please tell the creator of this site, include this: \n" + navigator.userAgent);
-        }
-    }
-</script>
