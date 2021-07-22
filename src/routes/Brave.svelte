660f045be4f93047b6434d8dcbce7faae6d560dd
Oxygemo <alej0hio2007@gmail.com>
2021-07-22 22:00:44 +0200

    Fix brave
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/routes/Brave.svelte
+++ src/routes/Brave.svelte
@@ -1,13 +1,13 @@
 <div id="input">
-    <input type="text" id="userInput" value="BRAVE BROWSER®">
+    <input type="text" id="userInput" value="BRAVE BROWSER">
 </div>
 
 <div id="text">
-    <p>For me, it's the <b name="insert">BRAVE BROWSER®</b>. The best internet browser. I even download the <b name="insert">BRAVE BROWSER®</b> on my phone, tablet, laptop, and desktop because the <b name="insert">BRAVE BROWSER®</b> DEVELOPMENT TEAM is so friendly and more than willing to oblige.</p><br>
+    <p>For me, it's the <b name="insert">BRAVE BROWSER</b>. The best internet browser. I even download the <b name="insert">BRAVE BROWSER</b> on my phone, tablet, laptop, and desktop because the <b name="insert">BRAVE BROWSER</b> is so user friendly and helps fill my wallets with BAT (Basic Attention Token)</p><br>
 
-    <p>One time I asked for 3 ads to be blocked and they blocked three. I said, "Wow, three for free!" and the nice friendly <b name="insert">BRAVE BROWSER®</b> worker laughed and said, "I'm going to call you 3-for-free!".</p><br>
+    <p>One time I asked for 3 ads to be served and they gave me three, WITH the BAT (Basic Attention Token). I said, "Wow, three for free!" and the nice friendly <b name="insert">BRAVE</b> dev laughed and said, "I'm going to call you 3-for-free!". But the best part is with BAT, it's not even free for me!</p><br>
 
-    <p>Now the <b name="insert">BRAVE BROWSER®</b> greets me with "hey it's 3-for-free!" and ALWAYS blocks every ad, cookie, and tracker. It's such a fun and cool atmosphere with my <b name="insert">BRAVE BROWSER®</b>, I use it at least 7 times a week for my daily browsing needs which include work, pornography, school, and stalking my whore ex wife on facebook.</p><br>
+    <p>Now the <b name="insert">BRAVE BROWSER</b> greets me with "hey it's 3-for-free!" and ALWAYS blocks every ad, cookie, and tracker. It's such a fun and cool atmosphere with my <b name="insert">BRAVE BROWSER</b>,  I use it at least 7 times a week for my daily browsing needs which include work, shitposting, school, and stalking my ex on facebook (without the trackers!).</p><br>
 
-    <p>I even use my <b name="insert">BRAVE BROWSER®</b> to browse Read It®, it's amazing! What a great <b name="insert">BRAVE BROWSER®</b>.</p>
+    <p>What a great <b name="insert">BRAVE BROWSER</b></p>
 </div>
