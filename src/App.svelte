d18b6151e6cbc11d4be37f9293221d8065af1e27
akisblack <41871089+akisblack@users.noreply.github.com>
2021-07-27 18:26:51 +0300

    change license

--- src/App.svelte
+++ src/App.svelte
@@ -58,7 +58,7 @@
                 <a href="https://github.com/cantinadevs/browsercopypasta"><i class="mdi mdi-github"></i></a>
                 <a href="https://cantinadevs.github.io"><i class="mdi mdi-web"></i></a>
                 <hr>
-                <p><i class="mdi mdi-copyright"></i> This site is maintained by The Cantina Devs and is licensed under the MIT License</p>
+                <p><i class="mdi mdi-copyright"></i> This site is maintained by The Cantina Devs and is licensed under the <a href="https://github.com/akisblack/fair-license">Fair</a> License</p>
         </div>
     </footer>
 </main>
