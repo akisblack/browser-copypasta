61af6abb4d1dd383be7c061b1cbf0bf503326b70
akisblack <41871089+akisblack@users.noreply.github.com>
2021-08-07 14:25:28 +0300

    fucking odyssey

--- src/routes/brave.svelte
+++ src/routes/brave.svelte
@@ -1,20 +1,15 @@
-<script>
-    let browserName = "BRAVE BROWSER";
-    let browserNameHTML = `<b name="insert">${browserName}</b>`;
-</script>
-
 <div id="input">
-    <input type="text" id="userInput" value="{browserName}">
+    <input type="text" id="userInput" value="BRAVE BROWSER">
 </div>
 
 <div id="text">
     <pre>
-For me, it's the {@html browserNameHTML}. The best internet browser. I even download the {@html browserNameHTML} on my phone, tablet, laptop, and desktop because the {@html browserNameHTML} is so user friendly and helps fill my wallets with BAT (Basic Attention Token)
+For me, it's the <b name="insert">BRAVE BROWSER</b>. The best internet browser. I even download the <b name="insert">BRAVE BROWSER</b> on my phone, tablet, laptop, and desktop because the <b name="insert">BRAVE BROWSER</b> is so user friendly and helps fill my wallets with BAT (Basic Attention Token)
 
-One time I asked for 3 ads to be served and they gave me three, WITH the BAT (Basic Attention Token). I said, "Wow, three for free!" and the nice friendly {@html browserNameHTML} dev laughed and said, "I'm going to call you 3-for-free!". But the best part is with BAT, it's not even free for me!
+One time I asked for 3 ads to be served and they gave me three, WITH the BAT (Basic Attention Token). I said, "Wow, three for free!" and the nice friendly <b name="insert">BRAVE BROWSER</b> dev laughed and said, "I'm going to call you 3-for-free!". But the best part is with BAT, it's not even free for me!
 
-Now the {@html browserNameHTML} greets me with "hey it's 3-for-free!" and ALWAYS blocks every ad, cookie, and tracker. It's such a fun and cool atmosphere with my {@html browserNameHTML},  I use it at least 7 times a week for my daily browsing needs which include work, shitposting, school, and stalking my ex on facebook (without the trackers!).
+Now the <b name="insert">BRAVE BROWSER</b> greets me with "hey it's 3-for-free!" and ALWAYS blocks every ad, cookie, and tracker. It's such a fun and cool atmosphere with my <b name="insert">BRAVE BROWSER</b>,  I use it at least 7 times a week for my daily browsing needs which include work, shitposting, school, and stalking my ex on facebook (without the trackers!).
 
-What a great {@html browserNameHTML}
+What a great <b name="insert">BRAVE BROWSER</b>
     </pre>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,15 @@
-<script>
-    let browserName = "VIVALDI BROWSER";
-    let browserNameHTML = `<b name="insert">${browserName}</b>`;
-</script>
-
 <div id="input">
-    <input type="text" id="userInput" value="{browserName}">
+    <input type="text" id="userInput" value="VIVALDI BROWSER">
 </div>
 
 <div id="text">
     <pre>
-For me, it's the {@html browserNameHTML}. The best internet browser. I even download the {@html browserNameHTML} on my phone, tablet, laptop, and desktop because the {@html browserNameHTML} is so user friendly and helps fill my workflow with awesome tab features (Tab Stacks & Tab Tilling)
+For me, it's the <b name="insert">VIVALDI BROWSER</b>. The best internet browser. I even download the <b name="insert">VIVALDI BROWSER</b> on my phone, tablet, laptop, and desktop because the <b name="insert">VIVALDI BROWSER</b> is so user friendly and helps fill my workflow with awesome tab features (Tab Stacks & Tab Tilling)
     
-One time I asked for 3 tab stacks, WITH the tab tilling (multiple sites open at once on a screen). I said, "Wow, three for free!" and the nice friendly {@html browserNameHTML} dev laughed and said, "I'm going to call you 3-for-free!". But the best part is with tab tilling, it's not even free for me!
+One time I asked for 3 tab stacks, WITH the tab tilling (multiple sites open at once on a screen). I said, "Wow, three for free!" and the nice friendly <b name="insert">VIVALDI BROWSER</b> dev laughed and said, "I'm going to call you 3-for-free!". But the best part is with tab tilling, it's not even free for me!
     
-Now the {@html browserNameHTML} greets me with "hey it's 3-for-free!" and ALWAYS gets me a tab stack, with a web panel on the side. It's such a fun and cool atmosphere with my {@html browserNameHTML}, I use it at least 7 times a week for my daily browsing needs which include work, shitposting, school, and stalking my ex on facebook (with her instagram tiled to the side!).
+Now the <b name="insert">VIVALDI BROWSER</b> greets me with "hey it's 3-for-free!" and ALWAYS gets me a tab stack, with a web panel on the side. It's such a fun and cool atmosphere with my <b name="insert">VIVALDI BROWSER</b>, I use it at least 7 times a week for my daily browsing needs which include work, shitposting, school, and stalking my ex on facebook (with her instagram tiled to the side!).
     
-What a great {@html browserNameHTML}
+What a great <b name="insert">VIVALDI BROWSER</b>
     </pre>
 </div>
